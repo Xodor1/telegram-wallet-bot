8884e74027e5063cea4655e24de7c633338eb859
Xodor1 <garanteks51@gmail.com>
2025-05-21 18:23:27 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("btc").innerText = "0.0023 BTC";
-    document.getElementById("eth").innerText = "0.12 ETH";
-    document.getElementById("usdt").innerText = "134.5 USDT";
-});
