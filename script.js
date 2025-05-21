eff0f26b392b878ee7fd78cd0ac4daa1d5617253
Xodor1 <garanteks51@gmail.com>
2025-05-21 04:35:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btc").innerText = "0.0023 BTC";
+    document.getElementById("eth").innerText = "0.12 ETH";
+    document.getElementById("usdt").innerText = "134.5 USDT";
+});
