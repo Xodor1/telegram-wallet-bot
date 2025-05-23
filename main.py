7b69dc7b4f032a81080d1ac20a329ec6fc4675b7
Xodor1 <garanteks51@gmail.com>
2025-05-24 00:29:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
     webhook_path = os.environ.get("WEBHOOK_PATH") or "bot-hook"
     render_url = os.environ.get("RENDER_EXTERNAL_URL") or "https://telegram-wallet-bot-2lyl.onrender.com"
     webhook_url = f"{render_url}/{webhook_path}"
-
     start_bot(token, webhook_url)
